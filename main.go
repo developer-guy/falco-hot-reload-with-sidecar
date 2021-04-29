03ebd9d58170dfcb2370581e8176b230cf077aec
Issif <issif_github@gadz.org>
2021-04-29 23:29:03 +0200

    refactoring
    Signed-off-by: Issif <issif_github@gadz.org>

--- main.go
+++ main.go
@@ -4,115 +4,82 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"syscall"
 	"path/filepath"
+	"syscall"
+
 	"github.com/fsnotify/fsnotify"
-	ps "github.com/mitchellh/go-ps"
 )
 
-func main(){
-    rootDir := os.Getenv("FALCO_ROOTDIR")
-    
-    sigs := make(chan os.Signal, 1)
-    signal.Notify(sigs, syscall.SIGHUP)
+func main() {
+	rootDir := os.Getenv("FALCO_ROOTDIR")
 
-    watcher, err := fsnotify.NewWatcher()  
-    if err != nil {
-        log.Fatalf("could not create watcher:%v\n", err)
-    }
-    defer watcher.Close()
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGHUP)
 
-    go func() {
-        for {
-		select {
-		case event, ok  := <-watcher.Events:
-            if !ok {
-              return
-            }
-			// k8s configmaps uses symlinks, we need this workaround.
-            // original configmap file is removed
-			if event.Op == fsnotify.Remove {
-                // remove the watcher since the file is removed
-				watcher.Remove(event.Name)
-                // add a new watcher pointing to the new symlink/file
-				watcher.Add(event.Name)
-			    
-                log.Printf("event:%v\n", event)
-                
-                falcoPID, err := findPidOfFalcoProcess()
-                if err != nil {
-                  log.Fatalf("could not found pid of falco process:%v\n", err)
-                }
-
-                reloadProcess(falcoPID, syscall.SIGHUP)
-            }
-			// also allow normal files to be modified and reloaded.
-			if event.Op == fsnotify.Write {
-                log.Printf("event:%v\n", event)
-                falcoPID, err := findPidOfFalcoProcess()
-                if err != nil {
-                  log.Fatalf("could not found pid of falco process:%v\n", err)
-                }
-
-                reloadProcess(falcoPID, syscall.SIGHUP)
-            }
-		case err, ok := <-watcher.Errors:
-            if !ok {
-                return
-            }
-            
-            log.Fatalf("error:%v\n", err)
-		}
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatalf("could not create watcher:%v\n", err)
 	}
-}()
-    
-    // starting at the root of the project, walk each file/directory searching for
+	defer watcher.Close()
+
+	go func() {
+		for {
+			select {
+			case event, channelIsStillOpened := <-watcher.Events:
+				if !channelIsStillOpened {
+					return
+				}
+				// k8s configmaps uses symlinks, we need this workaround.
+				// original configmap file is removed
+				if event.Op == fsnotify.Remove || event.Op == fsnotify.Write {
+					// remove the watcher since the file is removed
+					watcher.Remove(event.Name)
+					// add a new watcher pointing to the new symlink/file
+					watcher.Add(event.Name)
+
+					log.Printf("event:%v\n", event)
+
+					if err := reloadProcess(findPidOfFalcoProcess(), syscall.SIGHUP); err != nil {
+						log.Printf("could not reload falco: %v\n", err)
+						return
+					}
+				}
+			case err, channelIsStillOpened := <-watcher.Errors:
+				if !channelIsStillOpened {
+					return
+				}
+				log.Fatalf("error:%v\n", err)
+			}
+		}
+	}()
+
+	// starting at the root of the project, walk each file/directory searching for
 	// directories
-	if err := filepath.Walk(rootDir,func(path string, info os.FileInfo, err error) error {
-        // since fsnotify can watch all the files in a directory, watchers only need
-	    // to be added to each nested directory
-	    if !info.Mode().IsDir() {
-          log.Printf("starting to watch %s\n", path)
-		  return watcher.Add(path)
-	    }
+	if err := filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
+		// since fsnotify can watch all the files in a directory, watchers only need
+		// to be added to each nested directory
+		if !info.Mode().IsDir() {
+			log.Printf("starting to watch %s\n", path)
+			return watcher.Add(path)
+		}
 
-        return nil
-      },
-    ); err != nil {
-	    log.Fatalf("could not walk directory %s:%v\n",rootDir, err)
-    }
-    <- make(chan struct{})
+		return nil
+	},
+	); err != nil {
+		log.Fatalf("could not walk directory %s:%v\n", rootDir, err)
+	}
+	<-make(chan struct{})
 }
 
-func reloadProcess(pid int, signal syscall.Signal){
-                
-     log.Printf("SIGHUP signal sending to PID %d\n",pid)
-     
-     
-     
-     err = syscall.Kill(pid, signal)
-     if err != nil {
-       log.Fatalf("could not send SIGHUP signal:%v\n", err)
-     }
-     
-     log.Printf("SIGHUP signal send to PID %d\n", pid)
+func reloadProcess(pid int, signal syscall.Signal) error {
+	log.Printf("SIGHUP signal sending to PID %d\n", pid)
+
+	err := syscall.Kill(pid, signal)
+	if err != nil {
+		log.Printf("could not send SIGHUP signal:%v\n", err)
+		return err
+	}
+
+	log.Printf("SIGHUP signal send to PID %d\n", pid)
+	return nil
 }
-
-func findPidOfFalcoProcess() (int, error) {
-    processes, err := ps.Processes()
-
-    if err != nil {
-        return -1, err
-    }
-
-    var pid int
-    for _ , p := range processes {
-            if p.Executable() == "falco" {
-                log.Printf("executable %s found with pid %d\n", p.Executable(), p.Pid())
-                pid = p.Pid() 
-            }
-    }
-
-    return pid, nil
-}
-
